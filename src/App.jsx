c3394ac3f1027ca441bc9e184d4dcc1e40c490cd
Khaldoun-Amatoury <khaldounamatoury@gmail.com>
2024-01-08 13:29:58 +0200

    removed comments and polished

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 import Results from "./components/Results.jsx";
-//problem: there is a bug which is when we enter negative duration or 0 duration value the app crashes
-//solution: we want to make sure that the results component is output conditionally, it should only output if we have valid user input
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -19,7 +17,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: +newValue, //we added a plus before new value because there is a bug in the website where when we are adding numbers in the input together they are strings not numbers even though we put type= numbers they are still strings when calculated so they are being concatenated together like "1200" + "1300" = "12001300" so we need to transform them into numbers that's why we did that, the plus sign forces the conversion of the string value into a number int value
+        [inputIdentifier]: +newValue,
       };
     });
   }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,31 +1,9 @@
 export default function UserInput({ onChange, userInput }) {
-  //IMP
-  //We lifted this userInput state up from the UserInput component to the app component as results components needs some functionality from userinput components so they are both linked in app so the state and the function handler should be in app component
-
-  /*
-  // here we could've made a state for each input but it is not a good practice so we made one state which binds all inputs together into one state with one handle function
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestmnet: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
-*/
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          {/* here we made the onChange take a arrow function because when you put onChange it creates with it a event object which only takes on parameter so it does not take the input identifier so that's why we didn't put it as onChange = {handleChange} so first we made a arrow  funcion and then put for handleChange a parameter which is a hard coded one initialinvestment and then the other parameter which is made by the help of event object which is event.target.value */}
           <input
             type="number"
             required
